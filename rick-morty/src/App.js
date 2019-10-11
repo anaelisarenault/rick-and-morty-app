d0072d0192bdcef9657f1a001dbe364adf662be9
Ana Renault <anaelisarenault@hotmail.comm>
2019-10-11 18:30:58 -0300

    add axios

--- rick-morty/src/App.js
+++ rick-morty/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Search from './components/layout/Search';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('https://rickandmortyapi.com/api/').then(res => console.log(res.data))
+  }
   render() {
     return (
-    
       <div className="App">
         <div className="container"> 
           <Search />
